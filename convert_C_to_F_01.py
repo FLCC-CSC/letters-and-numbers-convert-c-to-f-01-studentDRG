5d50b196bf6dde6ef45c89767c109fb29a5b5cc0
studentDRG <info@oscqr.com>
2025-02-16 21:57:58 -0500

    Update convert_C_to_F_01.py

--- convert_C_to_F_01.py
+++ convert_C_to_F_01.py
@@ -1,90 +1,16 @@
-# FILE NAME - convert_C_to_F_01.py
+# FILE NAME - convertCF01.py
 
-# NAME: 
-# DATE: 
-# BRIEF DESCRIPTION:  
+# NAME - 
+# DATE - 
+# DESCRIPTION - 
 
+def main():
+    convertCF()
 
+def convertCF():
+    temp = float(input('Enter a temperature in Celsius: '))
+    newTemp = temp * 9 / 5 + 32
+    print()
+    print(temp, 'degrees Celsius is', newTemp, 'degrees Fahrenheit.')
 
-# 1. Make sure you fill out the comments above
-# 2. Write your code in the proper spot
-# 3. Be sure to answer the Reflection Questions below
-# 4. The Sample Output has been included in this code for your convenience
-
-
-
-
-    
-# Don't forget to cast user input as a float!
-    
-########## ENTER YER CODE BELOW THIS LINE ##########
-
-
-
-
-
-
-
-
-
-
-########### END YER CODE ABOVE THIS LINE ###########
-
-
-
-
-
-########################################
-#          SAMPLE OUTPUT
-########################################
-
-'''
-
-Enter a temperature in Celsius: 100
-
-100.0 degrees Celsius is 212.0 degrees Fahrenheit.
-
-'''
-
-
-
-'''
-
-Enter a temperature in Celsius: -40
-
--40.0 degrees Celsius is -40.0 degrees Fahrenheit.
-
-'''
-
-
-
-'''
-
-Enter a temperature in Celsius: 1
-
-1 degrees Celsius is 33.8 degrees Fahrenheit.
-
-'''
-
-
-
-########################################
-#          REFLECTION QUESTIONS
-########################################
-
-'''
-
-1. What does `float` mean?
-
-
-
-
-
-2. Why do you think it is important to use `float` as opposed to
-   a different type of variable type?
-
-
-
-
-
-'''
+main()

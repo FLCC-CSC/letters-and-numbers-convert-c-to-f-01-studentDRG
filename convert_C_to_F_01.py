29a1c10c608b467d98b8373e8321fa5135409af3
studentDRG <info@oscqr.com>
2025-02-20 03:31:02 +0000

    added 7

--- convert_C_to_F_01.py
+++ convert_C_to_F_01.py
@@ -5,6 +5,6 @@
 # DESCRIPTION - 
 
 temp = float(input('Enter a temperature in Celsius: '))
-newTemp = temp * 9 / 5 + 3267
+newTemp = temp * 9 / 5 + 32
 print()
 print(temp, 'degrees Celsius is', newTemp, 'degrees Fahrenheit.')
